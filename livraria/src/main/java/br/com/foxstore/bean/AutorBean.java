bce2bac59b4aa600da3778fafa476e0853559f90
Andre-PC <andreigor.17@hotmail.com>
2021-03-14 22:47:06 -0300

    Configurando ambiente JSF

---
+++ livraria/src/main/java/br/com/foxstore/bean/AutorBean.java
@@ -0,0 +1,24 @@
+package br.com.foxstore.bean;
+
+import br.com.foxstore.dao.DAO;
+import br.com.foxstore.modelo.Autor;
+import javax.annotation.ManagedBean;
+
+
+
+
+@ManagedBean
+public class AutorBean {
+
+	private Autor autor = new Autor();
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void gravar() {
+		System.out.println("Gravando autor " + this.autor.getNome());
+
+		new DAO<Autor>(Autor.class).adiciona(this.autor);
+	}
+}
